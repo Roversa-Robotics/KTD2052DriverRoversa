2afef1dc8746222ec6473de4a62f8686959073e7
Eric Bredder <121258349+eb8ga@users.noreply.github.com>
2025-04-10 08:19:07 -0400

    Update KTD2052DriverRoversa.cpp

--- source/KTD2052DriverRoversa.cpp
+++ source/KTD2052DriverRoversa.cpp
@@ -118,11 +118,11 @@ void pattern_all(int regData) {
 }
 
 /* 
-    Cycles is number of cycles before watchdog times out.
-    You can periodically refresh the cycles before it times out.
-    When cycles times out, the chip fades to zero and goes to low current standby mode.
-    255 disables watchdog and runs until pattern generator mode is turned off.
-    Note: always write to the watchdog register twice.
+Cycles is number of cycles before watchdog times out.
+You can periodically refresh the cycles before it times out.
+When cycles times out, the chip fades to zero and goes to low current standby mode.
+255 disables watchdog and runs until pattern generator mode is turned off.
+Note: always write to the watchdog register twice.
 */ 
 void pattern_watchdog(int cycles) {
     i2c_write(SID,0x15,cycles);
